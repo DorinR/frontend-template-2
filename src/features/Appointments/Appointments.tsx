2aedca779b81f030c4c85d2346a22f900157d838
DorinsMac <dorin.rogov@groupecyberjustice.com>
2024-03-10 15:04:14 -0400

    testing deployed cors settings

--- src/features/Appointments/Appointments.tsx
+++ src/features/Appointments/Appointments.tsx
@@ -1,8 +1,13 @@
+import { useGetAuthor } from "../../apiHooks/author/useGetAuthor";
+
 export const Appointments = () => {
+  const authorData = useGetAuthor();
+
   return (
     <div>
       <h2>Appointment</h2>
       <p>The appointments page</p>
+      {authorData}
     </div>
   );
 };
